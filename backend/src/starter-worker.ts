29382358d396219c0875f6756d9d6cf652558a61
Md Ikbal <ikbal84336@gmail.com>
2026-08-31 19:47:56 +0530

    Learnt about intances of a container to scale a service

--- backend/src/starter-worker.ts
+++ backend/src/starter-worker.ts
@@ -86,7 +86,3 @@ process.on("SIGINT", () => {
 process.on("SIGTERM", () => {
   void shutdown("SIGTERM");
 });
-// await cacheWarmerQueue.getJobSchedulers();
-// logger.info({
-//   event: "CACHE_WARMER_SCHEDULER_STARTED",
-// });
